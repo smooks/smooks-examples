e03300a07403a288eec889eb69b31c8c45e658ca
Claude Mamo <823038+claudemamo@users.noreply.github.com>
2023-06-18 19:44:20 +0200

    chore: compile to Java 11 (#266)

--- java-to-edifact/src/main/java/org/smooks/examples/java2edifact/Main.java
+++ java-to-edifact/src/main/java/org/smooks/examples/java2edifact/Main.java
@@ -6,35 +6,35 @@
  * %%
  * Licensed under the terms of the Apache License Version 2.0, or
  * the GNU Lesser General Public License version 3.0 or later.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-or-later
- * 
+ *
  * ======================================================================
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ======================================================================
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -42,6 +42,9 @@
  */
 package org.smooks.examples.java2edifact;
 
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.JAXBException;
 import org.smooks.Smooks;
 import org.smooks.edifact.binding.d03b.*;
 import org.smooks.edifact.binding.service.*;
@@ -49,20 +52,17 @@ import org.smooks.io.payload.ByteSource;
 import org.smooks.io.payload.StringResult;
 import org.xml.sax.SAXException;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
 import javax.xml.namespace.QName;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
 
 public class Main {
-    
+
     public static void main(String[] args) throws IOException, SAXException, JAXBException {
         System.out.println("\n" + run());
     }
-    
+
     protected static String run() throws JAXBException, IOException, SAXException {
         // Build Java model        
         Interchange interchange = new Interchange().
@@ -130,7 +130,7 @@ public class Main {
         final Smooks smooks = new Smooks("smooks-config.xml");
         StringResult stringResult = new StringResult();
         smooks.filterSource(new ByteSource(byteArrayOutputStream.toByteArray()), stringResult);
-        
+
         return stringResult.getResult();
     }
 }
